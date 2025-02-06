1c525adb7f09fa3b69ea718956e3e12c02a172fd
Lancelot Prigent <lancelot@prigent.dev>
2025-02-06 08:50:03 +0100

    add examples

---
+++ examples/global/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from "@nestjs/core";
+import { StandardSchemaValidationPipe } from "../../dist";
+import { AppModule } from "./app.module";
+
+async function main() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new StandardSchemaValidationPipe());
+
+  await app.listen(3000);
+}
+
+main();
