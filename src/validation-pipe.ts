94933454419165c80b6c697f15b5d0b7d0b10f67
Lancelot Prigent <lancelot@prigent.dev>
2025-02-06 18:31:12 +0100

    feat: add exception factory option

--- src/validation-pipe.ts
+++ src/validation-pipe.ts
@@ -4,13 +4,22 @@ import {
   Injectable,
   PipeTransform,
 } from "@nestjs/common";
+import { StandardSchemaV1 } from "@standard-schema/spec";
 import {
   getStandardSchemaDTO,
   isStandardSchemaDTO,
 } from "./create-standard-schema-dto";
 
+export interface StandardSchemaValidationPipeOptions {
+  exceptionFactory?: (issues: readonly StandardSchemaV1.Issue[]) => unknown;
+}
+
 @Injectable()
 export class StandardSchemaValidationPipe implements PipeTransform {
+  constructor(
+    private readonly options: StandardSchemaValidationPipeOptions = {},
+  ) {}
+
   async transform(value: unknown, metadata: ArgumentMetadata) {
     const { metatype } = metadata;
 
@@ -20,22 +29,28 @@ export class StandardSchemaValidationPipe implements PipeTransform {
     const result = await schema["~standard"].validate(value);
 
     if (result.issues) {
-      const error = new BadRequestException(
-        result.issues.map((issue) => {
-          const path = issue.path
-            ?.map((p) => (typeof p === "object" && !!p ? p.key : p))
-            .join(".");
+      const exception =
+        this.options.exceptionFactory?.(result.issues) ??
+        this.createException(result.issues);
 
-          return {
-            message: issue.message,
-            path,
-          };
-        }),
-      );
-
-      throw error;
+      throw exception;
     }
 
     return result.value;
+  }
+
+  private createException(issues: readonly StandardSchemaV1.Issue[]) {
+    return new BadRequestException(
+      issues.map((issue) => {
+        const path = issue.path
+          ?.map((p) => (typeof p === "object" && !!p ? p.key : p))
+          .join(".");
+
+        return {
+          message: issue.message,
+          path,
+        };
+      }),
+    );
   }
 }
